0cbe4c7330393cb81349d64af3171e975c5a15d6
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-08-19 17:28:18 +0700

    controller layout

---
+++ backend/src/controllers/scoreController.js
@@ -0,0 +1,29 @@
+import Score from "../models/Score";
+
+export const getAllScore = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const browseScore = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const createScore = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const deleteScore = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
