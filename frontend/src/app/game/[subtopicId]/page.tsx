dece6e306e4b4521d109410458a5f1e953062864
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-08-30 16:36:52 +0700

    move GamePage to [subtopicID], map number game

--- frontend/src/app/game/[subtopicId]/page.tsx
+++ frontend/src/app/game/[subtopicId]/page.tsx
@@ -1,29 +1,52 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import GameNav from "./components/GameNav";
-import Game from "./components/Game";
-import Leaderboard from "./components/Leaderboard";
-import ModalTimeout from "./components/ModalTimeout";
-import ModalExit from "./components/ModalExit";
 
-export default function GamePage() {
+import GameNav from "../components/GameNav";
+import Game from "../components/Game";
+import Leaderboard from "../components/Leaderboard";
+import ModalTimeout from "../components/ModalTimeout";
+import ModalExit from "../components/ModalExit";
+
+import { SubtopicApi } from "@/lib/SubTopicApi";
+
+import { Subtopic } from "@/types/SubTopic";
+
+interface Props {
+	params: {
+		subtopicId: string;
+	};
+}
+
+export default function GamePage({ params }: Props) {
 	const router = useRouter();
+	const { subtopicId } = params;
+
 	const [reload, setReload] = useState<boolean>(false);
+
 	const [time, setTime] = useState<number>(30000);
 	const [start, setStart] = useState<boolean>(false);
+
 	const [showTimeout, setShowTimeout] = useState<boolean>(false);
 	const [showExit, setShowExit] = useState<boolean>(false);
 
-	useEffect(() => {
-		setTime(30000);
+	const [subtopic, setSubtopic] = useState<Subtopic>({
+		_id: "",
+		subtopicName: "",
+		time: 30000,
+		category: "",
+		topicId: "",
+	});
 
+	const [score, setScore] = useState<number>(0);
+
+	useEffect(() => {
 		const timeoutId = setTimeout(() => {
 			setStart(true);
 		}, 1000);
 
 		return () => clearTimeout(timeoutId);
-	}, [reload]);
+	}, []);
 
 	useEffect(() => {
 		if (time === 0) setShowTimeout(true);
@@ -37,10 +60,29 @@ export default function GamePage() {
 		return () => clearInterval(intervalId);
 	}, [start, time, showExit]);
 
+	useEffect(() => {
+		getSubtopic();
+	}, []);
+
+	async function getSubtopic() {
+		try {
+			const getSubtopic = await SubtopicApi.getSubtopicById(subtopicId);
+
+			setSubtopic(getSubtopic);
+			setTime(getSubtopic.time);
+
+			console.log(getSubtopic);
+		} catch (error) {
+			console.error("Failed to get Subtopic:", error);
+		}
+	}
+
 	function handleRestart(): void {
 		setShowTimeout(false);
 		setShowExit(false);
 		setReload(!reload);
+		setScore(0);
+		setTime(subtopic?.time || 30000);
 	}
 
 	function handleExit(): void {
@@ -51,17 +93,23 @@ export default function GamePage() {
 		<>
 			<div className="flex flex-col gap-8 h-screen">
 				<div>
-					<GameNav time={time} />
+					<GameNav time={time} score={score} />
 				</div>
 				<div className="flex gap-8 justify-center items-center h-11/12">
 					<Game
 						time={time}
+						subtopic={subtopic}
+						score={score}
+						setScore={setScore}
 						setShowExit={setShowExit}
 						handleRestart={handleRestart}
+						reload={reload}
+						setReload={setReload}
 					/>
 					<Leaderboard />
 				</div>
 				<ModalTimeout
+					score={score}
 					showTimeout={showTimeout}
 					handleRestart={handleRestart}
 					handleExit={handleExit}
--- frontend/src/app/game/components/Game.tsx
+++ frontend/src/app/game/components/Game.tsx
@@ -1,52 +1,72 @@
+import { useEffect, useState } from "react";
+import numberCrunching from "../utils/numberCrunching";
+
+import { Subtopic } from "@/types/SubTopic";
+import { Question } from "@/types/Question";
+
 export default function Game({
 	time,
+	subtopic,
+	score,
+	setScore,
 	handleRestart,
 	setShowExit,
+	reload,
+	setReload,
 }: {
 	time: number;
+	subtopic: Subtopic;
+	score: number;
+	setScore: Function;
 	handleRestart: Function;
 	setShowExit: Function;
+	reload: boolean;
+	setReload: Function;
 }) {
+	const [question, setQuestion] = useState<Question>();
+
+	useEffect(() => {
+		setQuestion(numberCrunching());
+	}, [reload]);
+
+	function handleScore(isCorrect: boolean) {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+		setReload(!reload);
+	}
+
 	return (
 		<div className="flex flex-col justify-between gap-6 w-3/6 tw-box">
 			<div className="flex flex-col items-center gap-4">
 				<h1>เกมส์ตอบคำถาม</h1>
 				<div className="flex justify-around w-full">
-					<h2>หมวดหมู่ -วิชาการ-</h2>
-					<h2>หัวข้อ -การเคลื่อนที่ในแนวเส้นตรง-</h2>
+					<h2>หมวดหมู่ {subtopic?.category}</h2>
+					<h2>หัวข้อ {subtopic?.subtopicName}</h2>
 				</div>
+				<h2>หัวข้อย่อย {subtopic?.subtopicName}</h2>
 			</div>
 			<div className="flex flex-col items-center gap-4">
-				<div className="w-2/3 h-40 bg-neutral-200"></div>
-				<h2>-โจทย์-</h2>
+				{/* <div className="w-2/3 h-40 bg-neutral-200"></div> */}
+				<h2>{question?.questionName}</h2>
 			</div>
 
 			<div className="flex flex-col items-center gap-4">
 				<div className="grid grid-cols-2 grid-rows-2 gap-2 w-1/2">
-					<button
-						disabled={time === 0}
-						className="tw-btn bg-accent border border-secondary shadow-md hover:bg-accent-hover"
-					>
-						40
-					</button>
-					<button
-						disabled={time === 0}
-						className="tw-btn bg-accent border border-secondary shadow-md hover:bg-accent-hover"
-					>
-						1
-					</button>
-					<button
-						disabled={time === 0}
-						className="tw-btn bg-accent border border-secondary shadow-md hover:bg-accent-hover"
-					>
-						9
-					</button>
-					<button
-						disabled={time === 0}
-						className="tw-btn bg-accent border border-secondary shadow-md hover:bg-accent-hover"
-					>
-						8
-					</button>
+					{question?.option.map((item, index) => {
+						return (
+							<button
+								key={index}
+								disabled={time === 0}
+								onClick={() => {
+									handleScore(item.isCorrect);
+								}}
+								className="tw-btn bg-accent border border-secondary shadow-md hover:bg-accent-hover"
+							>
+								{item.text}
+							</button>
+						);
+					})}
 				</div>
 				<div className="flex gap-2">
 					<button
