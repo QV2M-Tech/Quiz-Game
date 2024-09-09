c92037acdbfaa047cfa5915b5c9c203f4a45c072
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-09-09 22:14:33 +0700

    FE delete console.log, delete don't use file, edit axios to use .env

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios, {
 } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-	baseURL: "http://localhost:6969/api",
+	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 	timeout: 10000,
 	headers: {
 		"Content-Type": "application/json",
