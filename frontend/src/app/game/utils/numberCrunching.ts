1570c2325bb83c8ab525f2443c08fd9208ebc6fc
mezealot24 <wiriya.on@gmail.com>
2024-09-27 00:07:40 +0700

    fix fastmath_id

--- frontend/src/app/game/utils/numberCrunching.ts
+++ frontend/src/app/game/utils/numberCrunching.ts
@@ -42,7 +42,7 @@ export default function numberCrunching(): Question {
 	options.sort(() => Math.random() - 0.5);
 
 	const question: Question = {
-		_id: "66f54da5b7e39f5480d16cce",
+		_id: "66f3e854b4bc1daba5211ca3",
 		subtopicId: "66f54da5b7e39f5480d16cce",
 		questionName: `${num1} ${op} ${num2}`,
 		option: options,
